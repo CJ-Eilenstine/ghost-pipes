0f4214f122d84bb3f84a746587e21dbda62e82a6
jago-1009 <125377105+jago-1009@users.noreply.github.com>
2024-11-09 13:08:11 -0500

    Finished Biome Page and restyling
    Finished Biome page and restyled pages to fit the color palette.

--- src/app/biome/[state]/page.js
+++ src/app/biome/[state]/page.js
@@ -2,35 +2,146 @@
 import stateStyles from "./state.module.css";
 import Link from "next/link";
 import getStates from "@/hooks/getStates";
+import Image from "next/image";
+import GhostPipe from "../../../assets/images/ghost-pipe-biome.jpg";
 export default function State({ params }) {
-    const stateData = getStates();
-    const statesRaw = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
-    const statesNoPipes = ["arizona", "new mexico", "colorado", "utah", "nevada", "wyoming", "south dakota"]
-    let states = statesRaw.map((state) => state.toLowerCase());
-    let stateRaw = (params.state);
-    let state = stateRaw.toLowerCase();
-    if (states.includes(state)) {
+   const slug = params.state
+  const stateData = getStates();
+  const statesRaw = [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+    "Wyoming",
+  ];
+  const statesNoPipes = [
+    "arizona",
+    "new mexico",
+    "colorado",
+    "utah",
+    "nevada",
+    "wyoming",
+    "south dakota",
+  ];
+  let states = statesRaw.map((state) => state.toLowerCase());
+  let stateRaw = params.state;
+  let state = stateRaw.toLowerCase();
+  if (states.includes(state)) {
     if (statesNoPipes.includes(state)) {
-        return (
-            <main>
-                <h1>Unfortunately, {stateData.capitalizeFirstLetter(stateData.state)} has no Ghost Pipes. If they did, you could find them in deep shady woods in low to moderate elevations.</h1>
-            </main>
-        );
-    } else {
-        return (
-            <main>
-                <h1>{stateData.capitalizeFirstLetter(stateData.state)} has Ghost Pipes! you can find them in deep shady woods in low to moderate elevations.</h1>
-            </main>
-        );
-    }
-} else {
-    return (
-        <main>
-            <h1>We're sorry, The state you entered ({stateData.state}) does not exist. Please try again.</h1>
-            <input type="text" value={stateData.state} onChange={(e) => stateData.changeState(e.target.value)} />
-            <Link href={`/biome/${stateData.state}`}><button>Submit</button></Link>
+      return (
+        <main className={stateStyles.main}>
+          <div className={stateStyles.container}>
+            <h1>
+              Unfortunately, {stateData.capitalizeFirstLetter(slug)}{" "}
+              has no Ghost Pipes.
+            </h1>
+            <p>
+              If they did, you could find them in deep shady woods in low to
+              moderate elevations.
+            </p>
+          </div>
         </main>
+      );
+    } else {
+      return (
+        <main className={stateStyles.main}>
+          <div className={stateStyles.container}>
+            <h1>
+              {stateData.capitalizeFirstLetter(slug)} has Ghost
+              Pipes!
+            </h1>
+            <p>
+              you can find them in deep shady woods in low to moderate
+              elevations.
+            </p>
+            <Image
+              className={stateStyles.image}
+              src={GhostPipe}
+              alt="Ghost Pipe"
+              width={500}
+            />
+          </div>
+          <svg viewBox="0 0 390.3 414.4" width="0" height="0">
+            <clipPath
+              id="blob3"
+              clipPathUnits="objectBoundingBox"
+              transform="scale(0.002562, 0.002413)"
+            >
+              <path
+                class="st0"
+                d="M276.8,56.9c23.5,25.9,33.9,61.3,53.7,95.3c19.8,33.9,49,66.4,57.3,104.6c8.2,38.2-4.5,82.3-35.2,102.2
+	c-30.6,20-79.2,15.8-121.3,23.7c-42.1,7.8-77.8,27.6-118.6,31.1c-40.9,3.5-86.9-9.3-99.3-41.4c-12.3-32.1,8.9-83.4,8.9-127.5
+	c0-44.2-21.1-81.2-22.3-121.6C-1.1,82.9,17.9,39,51.2,17.6C84.6-3.9,132.3-2.9,174.5,5.8S253.3,30.9,276.8,56.9z"
+              />
+            </clipPath>
+          </svg>
+        </main>
+      );
+    }
+  } else {
+    return (
+      <main className={stateStyles.main}>
+        <div className={stateStyles.container}>
+          <h1>
+            We're sorry, The state you entered ({slug}) does not
+            exist. Please try again.
+          </h1>
+          <input
+            className={stateStyles.input}
+            type="text"
+            value={stateData.state}
+            onChange={(e) => stateData.changeState(e.target.value)}
+          />
+          <Link href={`/biome/${stateData.state}`}>
+            <button className={stateStyles.button}>Submit</button>
+          </Link>
+        </div>
+      </main>
     );
-}
-    
+  }
 }
--- src/app/biome/page.js
+++ src/app/biome/page.js
@@ -7,11 +7,13 @@ export default function Biome() {
   const stateData = getStates();
   return (
     
-    <main>
+    <main className={biomeStyles.main}>
+      <div className={biomeStyles.container}>
       <h1>Biome</h1>
       <p>Ghost Pipes exist in many different biomes and regions throughout the world, especially in the United States! It has a large geographic distribution, coasting around most of the country! To see if your state has Ghost Pipes, enter your state below!</p>
-      <input type="text" value={stateData.state} onChange={(e) => stateData.changeState(e.target.value)} />
-      <Link href={`/biome/${stateData.state}`}><button>Submit</button></Link>
+      <input className={biomeStyles.input} type="text" value={stateData.state} onChange={(e) => stateData.changeState(e.target.value)} />
+      <Link href={`/biome/${stateData.state}`}><button className={biomeStyles.button}>Submit</button></Link>
+      </div>
     </main>
   );
 }
