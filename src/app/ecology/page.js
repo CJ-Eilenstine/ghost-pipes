aefd282fb7d44873a614d87d783ebd863855ab36
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-11-12 22:17:59 -0500

    Ecology
    Base Ecology page done, planning out how to add nested route now.

--- src/app/ecology/page.js
+++ src/app/ecology/page.js
@@ -1,9 +1,23 @@
 import ecologyStyles from "./ecology.module.css";
+import Image from "next/image";
+import ecologyImg from "@/assets/images/ghost-pipe-ecology.jpg";
 
 export default function Ecology() {
   return (
-    <main>
+    <main className={ecologyStyles.mainEco}>
       <h1>Ecology</h1>
+      <p>
+        Ecology is a branch of science that studies the relationships between
+        organisms and their environments.
+      </p>
+      <p>{`Ghost pipes don't photosynthesize normally like most other plants, instead they get nutrients from the fungi that grow near tree roots. The fungi are in a symbiotic relationship with the tree where the fungi provide minerals like phosphate and the tree provides some of it's energy from photosynthesis. The ghost pipe is in a parasitic relationship with these fungi and take the energy they get from the tree.`}</p>
+      <p>Lorem</p>
+      <Image
+        src={ecologyImg}
+        width={300}
+        height={300}
+        alt="Ghost pipes near tree to represent parasitic relationship."
+      ></Image>
     </main>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -4,27 +4,12 @@ import "./globals.css";
 import Nav from "@/components/Nav";
 import { StatesProvider } from "@/hooks/getStates";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Nav />
-        <StatesProvider>
-        {children}
-        </StatesProvider>
+        <StatesProvider>{children}</StatesProvider>
       </body>
     </html>
   );
