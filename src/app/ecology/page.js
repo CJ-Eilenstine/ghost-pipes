307e2a80b6da2912de99e54f579afbc3a5c3f213
CJ-Eilenstine <115022708+CJ-Eilenstine@users.noreply.github.com>
2024-11-13 21:18:22 -0500

    Home
    Fixed home page styling and progress on Ecology page.

--- src/app/ecology/page.js
+++ src/app/ecology/page.js
@@ -4,42 +4,71 @@ import ecologyStyles from "./ecology.module.css";
 import Image from "next/image";
 import ecologyImg from "@/assets/images/ghost-pipe-ecology.jpg";
 import Link from "next/link";
-import getForm from "@/hooks/getFormula";
+import { useState } from "react";
 
 export default function Ecology() {
-  const eqData = getForm();
+  const ObservationForm = ({ onSubmit }) => {
+    const [formData, setFormData] = useState({
+      location: "",
+      date: "",
+      weather: "",
+      observations: "",
+    });
 
-  return (
-    <main className={ecologyStyles.mainEco}>
-      <h1>Ecology</h1>
-      <p>
-        Ecology is a branch of science that studies the relationships between
-        organisms and their environments.
-      </p>
-      <p>{`Ghost pipes don't photosynthesize normally like most other plants, instead they get nutrients from the fungi that grow near tree roots. The fungi are in a symbiotic relationship with the tree where the fungi provide minerals like phosphate and the tree provides some of it's energy from photosynthesis. The ghost pipe is in a parasitic relationship with these fungi and take the energy they get from the tree. Ghost Pipes then use cellular respiration using the nutrients they get for energy.`}</p>
-      <p>Do you know the chemical formula for Cellular Respiration</p>
-      <form>
-        <div className={ecologyStyles.formDiv}>
-          <input type="text" value={eqData.one}></input>
-          <span>+</span>
-          <input type="text"></input>
-          <span>→</span>
-          <input type="text"></input>
-          <span>+</span>
-          <input type="text"></input>
-          <span>+</span>
-          <input type="text"></input>
-          <Link href={`/ecology/${correct}`}>
+    const handleChange = (e) => {
+      const { value } = e.target;
+      setFormData((prevData) => ({ ...prevData, value }));
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      onSubmit(formData);
+    };
+
+    return (
+      <main className={ecologyStyles.mainEco}>
+        <h1>Ecology</h1>
+        <p>
+          Ecology is a branch of science that studies the relationships between
+          organisms and their environments.
+        </p>
+        <p>{`Ghost pipes don't photosynthesize normally like most other plants, instead they get nutrients from the fungi that grow near tree roots. The fungi are in a symbiotic relationship with the tree where the fungi provide minerals like phosphate and the tree provides some of it's energy from photosynthesis. The ghost pipe is in a parasitic relationship with these fungi and take the energy they get from the tree. Ghost Pipes then use cellular respiration using the nutrients they get for energy.`}</p>
+        <p>Tell us about times you saw ghost pipes and what you noticed:</p>
+        <form>
+          <div className={ecologyStyles.formDiv}>
+            <input
+              type="text"
+              placeholder="Location"
+              value={formData.location}
+              onChange={handleChange}
+            ></input>
+
+            <input
+              type="date"
+              placeholder="Date"
+              value={formData.date}
+              onChange={handleChange}
+            ></input>
+
+            <input
+              type="text"
+              placeholder="Weather"
+              value={formData.weather}
+              onChange={handleChange}
+            ></input>
+
+            <textarea type="text" placeholder="Observations"></textarea>
+
             <input type="submit"></input>
-          </Link>
-        </div>
-      </form>
-      <Image
-        src={ecologyImg}
-        width={300}
-        height={300}
-        alt="Ghost pipes near tree to represent parasitic relationship."
-      ></Image>
-    </main>
-  );
+          </div>
+        </form>
+        <Image
+          src={ecologyImg}
+          width={300}
+          height={300}
+          alt="Ghost pipes near tree to represent parasitic relationship."
+        ></Image>
+      </main>
+    );
+  };
 }
